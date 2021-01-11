61cf9d3c1525ae176299cd5d9898db0997b857f0
Taoufiq-Imad <Taoufiq-Imad@users.noreply.github.com>
2021-01-11 12:22:05 +0100

    first commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using webProject.Model;
+using System.Collections.Generic;
+namespace webProject.Controllers
+{
+    public class TestController:Controller{
+        public IActionResult Index(){
+            IList<string> items=new List<string>();
+            items.Add("Imad");
+            items.Add("Oumayma");
+            items.Add("Hamza");
+            items.Add("Hajar");
+            ViewData["email"]="imad@taoufiq.ma";
+            ViewBag.contact="imad taoufiq";
+            return View(items);
+        }
+        public IActionResult List(){
+            IList<Student> students=new List<Student>();
+            students.Add(new Student{Id=1,Name="Imad",Score=60});
+            students.Add(new Student{Id=2,Name="Oumayma",Score=39});
+            students.Add(new Student{Id=3,Name="Hamza",Score=100});
+            return View(students);
+        }
+        public IActionResult A(){
+            return View();
+        }
+        public IActionResult B(){
+            return View("BB");
+        }
+    }
+}
